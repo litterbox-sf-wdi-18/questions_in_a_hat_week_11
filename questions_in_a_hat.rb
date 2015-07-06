768538ee90cc18043880dd34890b7b166787a2c3
Ilias Tsangaris <ilias@ga.co>
2015-07-06 01:25:06 -0700

    Update questions_in_a_hat.rb

--- questions_in_a_hat.rb
+++ questions_in_a_hat.rb
@@ -2,13 +2,13 @@ require_relative 'lib/question_picker'
 require_relative 'lib/assets/awk_turtle'
 require_relative 'lib/assets/question_list'
 
-wdi14 = Cohort.new(QUESTIONS)
+wdi = Cohort.new(QUESTIONS)
 
 QUESTIONS.count.times do
-  p "#{wdi14.random}"
+  p "#{wdi.random}"
   print ">> "
   input = gets.chomp
-  if wdi14.students.count == 0
+  if wdi.students.count == 0
     AwkwardTurtle.animate
   end
 end
